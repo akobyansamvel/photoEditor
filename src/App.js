01dee080942956febdb8b502e7acecf3ee330212
akobyansamvel <s.o.akobian@stud.mospolytech.ru>
2024-03-14 10:19:47 +0300

    загрузка изображений

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+ 
+  const [image, setImage] = useState();
+  const [imageURL, setImageURL] = useState('/no_photo.jpg'); // Изменено здесь
+  const fileReader = new FileReader();
+  fileReader.onloadend = () => {
+    setImageURL(fileReader.result);
+  };
+  const handleOnChange = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    setImage(file);
+    fileReader.readAsDataURL(file);
+}
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className='uploader'>
+          <img
+          src={imageURL}
+          className="uploader__preview"
+          alt="preview"/>
+         <div className="uploader__file-name">{image ? image.name : ""}</div>
+            <label
+            htmlFor="loader-button"
+            className="uploader__button">
+              Загрузить файл
+            </label>
+            <input
+              id="loader-button"
+              type="file"
+              className="uploader__upload-button"
+              onChange={handleOnChange}/>
+        </form>
       </header>
     </div>
   );
